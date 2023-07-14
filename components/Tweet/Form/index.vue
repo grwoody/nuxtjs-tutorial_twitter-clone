f36016eceaf0e9e1c1f7b8b016466c3b668bf893
grwoody <gwoodenr@gmail.com>
2023-07-14 09:49:08 -0700

    updates tweet form to post to db

--- components/Tweet/Form/index.vue
+++ components/Tweet/Form/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     
-    <TweetFormInput :user="props.user" />
+    <TweetFormInput :user="props.user" @onSubmit="handleFormSubmit" />
 
   </div>
 
@@ -9,11 +9,22 @@
 
 <script setup>
 
+const { postTweet } = useTweets()
+
 const props = defineProps({
   user: {
     type: Object,
     required: true
   }
 })
+
+async function handleFormSubmit(data) {
+  try {
+    const response = await postTweet(data)
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 </script>
