ad8a500c8d6590eb3c0d937545baf3d993f1a11a
grwoody <gwoodenr@gmail.com>
2023-07-14 09:47:00 -0700

    adds textarea input for tweets

--- components/Tweet/Form/Input.vue
+++ components/Tweet/Form/Input.vue
@@ -7,12 +7,29 @@
         <img :src="props.user?.profileImage" alt="" class="inline-block w-10 h-10 rounded-full">
       </div>
 
+      <div class="w-full p-2">
+        <textarea class="w-full h-10 text-lg text-gray-900 placeholder:text-gray-400 bg-transparent border-0 dark:text-white focus:ring-0"
+          v-model="text"
+        >
+        </textarea>
+      </div>
+
+
+
     </div>
+
+    <div>
+        <button @click="handleFormSubmit">Tweet</button>
+      </div>
   </div>
 
 </template>
 
 <script setup>
+
+const emits = defineEmits(['onSubmit'])
+
+const text = ref('')
 
 const props = defineProps({
   user: {
@@ -20,5 +37,11 @@ const props = defineProps({
     required: true
   }
 })
+
+function handleFormSubmit() {
+  emits('onSubmit', {
+    text: text.value
+  })
+}
 
 </script>
